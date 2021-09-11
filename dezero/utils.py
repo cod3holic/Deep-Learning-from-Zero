74143ef37a37e8d9f550af2c2a242675e763b4bf
cod3holic <cod3holic@gmail.com>
2021-09-11 09:09:16 +0000

    bug fix

--- dezero/utils.py
+++ dezero/utils.py
@@ -1,13 +1,16 @@
+import os
+import subprocess
+
 def _dot_var(v, verbose=False):
     name = '' if v.name is None else v.name
     if verbose and v.data is not None:
         if v.name is not None:
             name += ':'
         name += str(v.shape) + ' ' + str(v.dtype)
-    return f"{id(v)} [label='{name}', color=orange, style=filled]\n"
+    return f'{id(v)} [label="{name}", color=orange, style=filled]\n'
 
 def _dot_func(f):
-    txt = f"{id(f)} [label='{f.__class__.__name__}']\n"
+    txt = f'{id(f)} [label="{f.__class__.__name__}", color=lightblue, style=filled, shape=box]\n'
     
     dot_edge = '{} -> {}\n'
     for x in f.inputs:
@@ -38,3 +41,18 @@ def get_dot_graph(output, verbose=True):
                 add_func(x.creator)
 
     return 'digraph g {\n' + txt + '}'
+
+def plot_dot_graph(output, verbose=True, to_file='dot.png'):
+    dot_graph = get_dot_graph(output, verbose)
+    tmp_dir = os.path.join(os.path.expanduser('~'), '.dezero')
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+    graph_path = os.path.join(tmp_dir, 'tmp_graph.dot')
+
+    with open(graph_path, 'w') as f:
+        f.write(dot_graph)
+    
+    extension = os.path.splitext(to_file)[1][1:]
+    cmd = f'dot {graph_path} -T {extension} -o {to_file}'
+    print(cmd)
+    subprocess.run(cmd, shell=True)
